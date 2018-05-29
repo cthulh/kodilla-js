e2040eee1a44ec3f084cb16f619420e2062fadc9
Mariusz Borowiak <borowiakm85@gmail.com>
2018-05-29 10:51:29 +0100

    Add cell select func

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
 
   var flkty = new Flickity('.main-carousel', {
     cellAlign: 'center',
-    contain: true
+    contain: true,
+    autoPlay: true
+  });
+
+  // on click select that slide
+  flkty.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
+    // if not clicked
+    if ( !cellElement ) {
+      return;
+    }
+    document.querySelector('.is-selected').classList.remove('is-selected');
+    cellElement.classList.add('is-selected');
+    // select clicked cell
+    if ( typeof cellIndex == 'number' ) {
+      flkty.selectCell( cellIndex );
+    }
   });
 
 }());
